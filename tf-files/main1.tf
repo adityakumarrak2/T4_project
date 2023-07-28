525077beb8c353ed04e5264123ef3c331a3423cc
Aditya <adityakumarrak2@gmail.com>
2023-07-28 09:39:46 +0000

    changes #5

--- tf-files/main1.tf
+++ tf-files/main1.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "tictoc" {
-  bucket = "T4_project007 bucket"
+  bucket = "t4-project007-bucket"
   acl = "private"
   tags = {
-    Name = "T4_project007_S3__provisioning"
+    Name = "t4-project007-s3-provisioning"
   }
 }
