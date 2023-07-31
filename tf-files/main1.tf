1142e23363f7fabc4d2845957870f1e3d65861fb
Aditya <adityakumarrak2@gmail.com>
2023-07-31 18:06:47 +0000

    changes #12

--- tf-files/main1.tf
+++ tf-files/main1.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "tictoc" {
   bucket = "t4-project007-bucket"
   acl = "private"
   tags = {
-    Name = "t4-project007-s3-provisioning"
+    Name = "t4-project005-s3-provisioning"
   }
 }
--- tf-files/variable.tf
+++ tf-files/variable.tf
@@ -1,3 +1,57 @@
 variable "ec2_name" {
   type = string
 }
+
+variable "ec2_n" {
+  description = "The EC2 resource name"
+  type        = string
+default = "my_instance"
+}
+
+variable "ami_id" {
+  description = "The EC2 ami id"
+  type        = string
+default = "ami-024e6efaf93d85776"
+}
+
+variable "i_t" {
+  description = "The EC2 instance type"
+  type        = string
+default = "t2.micro"
+}
+
+variable "c" {
+  description = "count for ec2 vm(s) to be launched"
+  type        = string
+default = "1"
+}
+
+variable "sgid" {
+  description = "security group id"
+  type        = string
+default = "sg-0a954d1643b56f6fc"
+}
+
+variable "snid" {
+  description = "subnet id"
+  type        = string
+default = "subnet-05edbbbdbe21f3fbe"
+}
+
+variable "tag" {
+  description = "The EC2 instance type."
+  type        = string
+default = "T4_project007"
+}
+
+variable "sg-rn" {
+  description = "security  group resource name"
+  type        = string
+default = "my_security_group"
+}
+
+variable "vpcid" {
+  description = "vpc id"
+  type        = string
+default = "vpc-0ce62b15e4831ef46"
+}
